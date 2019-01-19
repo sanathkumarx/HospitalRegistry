798bebca6b26cc7742619a7491089dbf26d7c41a
Sanath Kumar <sanathkumarx@gmail.com>
2019-01-19 14:33:09 +0530

    Updated Database Schema(primarykeys ->mobile)

--- WebContent/Database/HospitaRegistry_mysql_create.sql
+++ WebContent/Database/HospitaRegistry_mysql_create.sql
@@ -1,27 +1,25 @@
 CREATE TABLE `Patient` (
-	`patID` int NOT NULL AUTO_INCREMENT,
-	`patName` varchar(50) NOT NULL,
 	`patMobile` numeric(15) NOT NULL,
+	`patName` varchar(50) NOT NULL,
 	`patPassword` varchar(20) NOT NULL,
 	`patLocation` varchar(20) NOT NULL,
-	PRIMARY KEY (`patID`)
+	PRIMARY KEY (`patMobile`)
 );
 
 CREATE TABLE `Doctor` (
-	`docID` int NOT NULL AUTO_INCREMENT,
-	`docName` varchar(50) NOT NULL,
 	`docMobile` numeric(15) NOT NULL,
+	`docName` varchar(50) NOT NULL,
 	`docPassword` varchar(20) NOT NULL,
 	`docLocation` varchar(20) NOT NULL,
 	`docSpecialization` varchar(20) NOT NULL,
 	`docDays` int NOT NULL,
-	PRIMARY KEY (`docID`)
+	PRIMARY KEY (`docMobile`)
 );
 
 CREATE TABLE `Appointment` (
 	`appID` int NOT NULL AUTO_INCREMENT,
-	`patID` int NOT NULL,
-	`docID` int NOT NULL,
+	`patMobile` numeric(15) NOT NULL,
+	`docMobile` numeric(15) NOT NULL,
 	`appDate` DATE NOT NULL,
 	`appStatus` bool NOT NULL,
 	PRIMARY KEY (`appID`)
@@ -30,25 +28,24 @@ CREATE TABLE `Appointment` (
 CREATE TABLE `Reports` (
 	`repID` int NOT NULL AUTO_INCREMENT,
 	`appID` int NOT NULL,
-	`techID` int NOT NULL,
-	`repStatus` bool NOT NULL,
+	`techMobile` numeric(15) NOT NULL,
 	`testName` varchar(50) NOT NULL,
+	`repStatus` bool NOT NULL,
 	PRIMARY KEY (`repID`)
 );
 
 CREATE TABLE `Technician` (
-	`techID` int NOT NULL AUTO_INCREMENT,
-	`techName` varchar(30) NOT NULL,
 	`techMobile` numeric(15) NOT NULL,
+	`techName` varchar(30) NOT NULL,
 	`techPassword` varchar(30) NOT NULL,
-	PRIMARY KEY (`techID`)
+	PRIMARY KEY (`techMobile`)
 );
 
-ALTER TABLE `Appointment` ADD CONSTRAINT `Appointment_fk0` FOREIGN KEY (`patID`) REFERENCES `Patient`(`patID`);
+ALTER TABLE `Appointment` ADD CONSTRAINT `Appointment_fk0` FOREIGN KEY (`patMobile`) REFERENCES `Patient`(`patMobile`);
 
-ALTER TABLE `Appointment` ADD CONSTRAINT `Appointment_fk1` FOREIGN KEY (`docID`) REFERENCES `Doctor`(`docID`);
+ALTER TABLE `Appointment` ADD CONSTRAINT `Appointment_fk1` FOREIGN KEY (`docMobile`) REFERENCES `Doctor`(`docMobile`);
 
 ALTER TABLE `Reports` ADD CONSTRAINT `Reports_fk0` FOREIGN KEY (`appID`) REFERENCES `Appointment`(`appID`);
 
-ALTER TABLE `Reports` ADD CONSTRAINT `Reports_fk1` FOREIGN KEY (`techID`) REFERENCES `Technician`(`techID`);
+ALTER TABLE `Reports` ADD CONSTRAINT `Reports_fk1` FOREIGN KEY (`techMobile`) REFERENCES `Technician`(`techMobile`);
 
