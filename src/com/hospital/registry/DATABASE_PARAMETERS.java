f9c7e90ee3d1341f9291c73cbc602e734983799d
Sanath Kumar <sanathkumarx@gmail.com>
2019-02-09 17:37:43 +0530

    Built war file

---
+++ src/com/hospital/registry/DATABASE_PARAMETERS.java
@@ -0,0 +1,7 @@
+package com.hospital.registry;
+
+public class DATABASE_PARAMETERS{
+    public static final String DB_USERNAME = "root";
+    public static final String DB_PASSWORD = "Kmit123$";
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/hospitalregistry";
+}
